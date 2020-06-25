964622e42873adae2dd5aa13bfc48ef848455df9
daxnet <daxnet@outlook.com>
2020-06-25 10:40:42 +0800

    Initial code commit.

---
+++ src/services/cluster-manager/Abacuza.ClusterManager.Common/Cluster.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abacuza.ClusterManager.Common
+{
+    public abstract class Cluster<TConnection>
+        where TConnection : ClusterConnection
+    {
+
+    }
+}
---
+++ src/services/cluster-manager/Abacuza.ClusterManager.Common/ClusterConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abacuza.ClusterManager.Common
+{
+    public class ClusterConnection
+    {   
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
