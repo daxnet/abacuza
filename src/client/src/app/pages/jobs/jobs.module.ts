15568e261d43aa0c9ea932c2f3df4e9f66ad0b38
daxnet <daxnet@outlook.com>
2020-10-15 15:48:34 +0800

    Fixing the build issue.

--- src/client/src/app/pages/jobs/jobs.module.ts
+++ src/client/src/app/pages/jobs/jobs.module.ts
@@ -3,18 +3,22 @@ import { CommonModule } from '@angular/common';
 import { JobRunnersComponent } from './job-runners/job-runners.component';
 import { JobsComponent } from './jobs.component';
 import { JobsRoutingModule } from './jobs-routing.module';
-import { NbCardModule, NbToastrModule } from '@nebular/theme';
+import { NbCardModule, NbToastrModule, NbSelectModule, NbInputModule } from '@nebular/theme';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { CreateJobRunnerComponent } from './job-runners/create-job-runner/create-job-runner.component';
 import { JobRunnerDetailsComponent } from './job-runners/job-runner-details/job-runner-details.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [JobsComponent, JobRunnersComponent, CreateJobRunnerComponent, JobRunnerDetailsComponent],
   imports: [
+    FormsModule,
     CommonModule,
     NbCardModule,
+    NbSelectModule,
+    NbInputModule,
     JobsRoutingModule,
     NbToastrModule.forRoot(),
     Ng2SmartTableModule,
