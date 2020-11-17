5e1cde6c6d8b6c817e236214aa55379bdcfb6275
daxnet <daxnet@outlook.com>
2020-11-17 16:43:11 +0800

    Fixing the build issue.

--- src/services/job-runners/publish.sh
+++ src/services/job-runners/publish.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export SolutionDir=$(PWD)/../../
 mkdir -p published
 cd Abacuza.JobRunners.Spark
 dotnet publish -c Release -f net5.0 -r linux-x64 -o ../published/Abacuza.JobRunners.Spark
