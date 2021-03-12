9f88c788f19aac401e56b8261385264d1b5d78f9
daxnet <daxnet@outlook.com>
2021-03-12 16:53:40 +0800

    Implementing the text input endpoint.

---
+++ src/services/endpoints/Abacuza.Endpoints.Input/TextInputEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using Abacuza.Common.Models;
+using Abacuza.Common.UIComponents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abacuza.Endpoints.Input
+{
+    [Endpoint("endpoints.input.text",
+        "Text Files", 
+        EndpointType.Input,
+        Description = "The endpoint that reads data from text files.")]
+    public sealed class TextInputEndpoint : Endpoint, IInputEndpoint
+    {
+        [FilePicker("fpTextFiles", "Text Files", AllowedExtensions = ".txt", AllowMultipleSelection = true, Tooltip = "Choose text files.")]
+        public List<S3File>? Files { get; set; }
+    }
+}
---
+++ src/services/job-runners/Abacuza.JobRunners.Spark.SDK/InputReaders/TextFileInputReader.cs
@@ -0,0 +1,27 @@
+﻿using Abacuza.Endpoints.Input;
+using Microsoft.Spark.Sql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Abacuza.JobRunners.Spark.SDK.InputReaders
+{
+    internal sealed class TextFileInputReader : InputReader<TextInputEndpoint>
+    {
+        protected override DataFrame ReadFromInternal(SparkSession sparkSession, TextInputEndpoint inputEndpoint, ProjectContext projectContext)
+        {
+            var textFiles = inputEndpoint
+                .Files
+                .Select(f => $"s3a://{f.Bucket}/{f.Key}/{f.File}")
+                .ToArray();
+
+            if (textFiles?.Length == 0)
+            {
+                throw new SparkRunnerException("No files could be read by the TextFileInputReader.");
+            }
+
+            return sparkSession.Read().Text(textFiles);
+        }
+    }
+}
